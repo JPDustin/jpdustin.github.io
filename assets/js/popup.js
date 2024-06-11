8c0fd70e6cb7168f9592495f158d5665a6ded9d6
JPDustin <97708492+JPDustin@users.noreply.github.com>
2024-06-10 22:13:45 -0400

    popup change

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,28 +1,3 @@
-// Function to set a cookie
-function setCookie(name, value, days) {
-    const date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    const expires = "expires=" + date.toUTCString();
-    document.cookie = name + "=" + value + ";" + expires + ";path=/";
-}
-
-// Function to get a cookie
-function getCookie(name) {
-    const cname = name + "=";
-    const decodedCookie = decodeURIComponent(document.cookie);
-    const ca = decodedCookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(cname) === 0) {
-            return c.substring(cname.length, c.length);
-        }
-    }
-    return "";
-}
-
 // Function to show the popup
 function showPopup() {
     const popup = document.getElementById("popup");
@@ -37,7 +12,7 @@ function hidePopup() {
 
 // Function to handle popup logic
 function handlePopup() {
-    const popupSeen = getCookie("popupSeen");
+    const popupSeen = sessionStorage.getItem("popupSeen");
     if (!popupSeen) {
         showPopup();
     }
@@ -45,14 +20,9 @@ function handlePopup() {
     const acknowledgeBtn = document.getElementById("acknowledge-btn");
     acknowledgeBtn.addEventListener("click", function() {
         hidePopup();
-        setCookie("popupSeen", "true", 1); // Set cookie to expire in 1 day
+        sessionStorage.setItem("popupSeen", "true");
     });
 }
-
-// Clear the popup cookie when the window is closed
-window.addEventListener("unload", function() {
-    document.cookie = "popupSeen=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-});
 
 // Execute the popup logic when the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", handlePopup);
