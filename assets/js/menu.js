4d9306fdcd3bf76a8e4822d34fdca7d04f4a2f98
JPDustin <97708492+JPDustin@users.noreply.github.com>
2024-06-12 19:41:00 -0400

    menu fix

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const menuToggle = document.querySelector(".menu-toggle");
-    const navMenu = document.querySelector("nav ul");
+    const navMenu = document.querySelector("nav");
 
     menuToggle.addEventListener("click", function() {
         navMenu.classList.toggle("active");
