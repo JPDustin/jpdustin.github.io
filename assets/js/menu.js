aa9595fa82e0459d435fb6a1d9107bbac931fe01
JPDustin <97708492+JPDustin@users.noreply.github.com>
2024-06-12 19:54:58 -0400

    menu fix

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,22 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     const menuToggle = document.querySelector(".menu-toggle");
-    const navMenu = document.querySelector("nav ul");
+    const navMenu = document.querySelector("nav");
 
-    console.log("Script loaded and DOMContentLoaded event fired");
-
-    if (menuToggle) {
-        menuToggle.addEventListener("click", function() {
-            navMenu.classList.toggle("active");
-            console.log("Menu toggled");
-        });
-    } else {
-        console.error("Menu toggle button not found");
-    }
-
-    // Optional: Close menu when clicking outside of it
-    document.addEventListener("click", function(event) {
-        if (!menuToggle.contains(event.target) && !navMenu.contains(event.target)) {
-            navMenu.classList.remove("active");
-        }
+    menuToggle.addEventListener("click", function() {
+        navMenu.classList.toggle("active");
     });
 });
