c664c0a130c81f43c6a86c62119f832d6e93e591
JPDustin <97708492+JPDustin@users.noreply.github.com>
2024-06-11 20:14:50 -0400

    Fixed menu

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,16 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("DOM fully loaded and parsed");
     const menuToggle = document.querySelector(".menu-toggle");
-    console.log("menuToggle:", menuToggle);  // Check if the element is null
     const navMenu = document.querySelector("nav");
-    console.log("navMenu:", navMenu);  // Should also log the nav element
 
-    if (menuToggle) {
-        menuToggle.addEventListener("click", function() {
-            navMenu.classList.toggle("active");
-            console.log("Toggle active class");
-        });
-    } else {
-        console.error("Menu toggle button not found on the page.");
-    }
+    menuToggle.addEventListener("click", function() {
+        navMenu.classList.toggle("active");
+    });
 });
