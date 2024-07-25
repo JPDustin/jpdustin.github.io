c8f007413ca4e81dd1a2a60446cd839013a7c2c0
JPDustin <97708492+JPDustin@users.noreply.github.com>
2024-07-24 21:00:45 -0400

    fix hamburger

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const menuToggle = document.querySelector(".menu-toggle");
-    const navMenu = document.querySelector("nav");
+    const navMenu = document.querySelector("nav ul");
 
     menuToggle.addEventListener("click", function() {
         navMenu.classList.toggle("active");
