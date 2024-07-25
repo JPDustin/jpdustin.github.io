fa1910bfc552bb27f6e545fe0407c2860fc6ed2d
JPDustin <97708492+JPDustin@users.noreply.github.com>
2024-07-24 22:16:56 -0400

    Delete assets directory

--- assets/js/menu.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.menu-toggle').addEventListener('click', function() {
-    var menu = document.querySelector('nav ul');
-    if (menu.classList.contains('active')) {
-        menu.classList.remove('active');
-    } else {
-        menu.classList.add('active');
-    }
-});
